2eab8bc8d663bfaf38c55721e964729a2751888f
varshnie <125456267+varshnie@users.noreply.github.com>
2023-03-17 11:05:16 +0530

    Header file

---
+++ Module.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef MODULE_NAME
+#define MODULE_NAME Player
+#endif
+
+
+
+#include <plugins/plugins.h>
+#include <tracing/tracing.h>
+
+//#include <core/core.h>
+
+#undef EXTERNAL
+#define EXTERNAL
+
